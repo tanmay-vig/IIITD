7b3e2a97b48d87a32d42547fadc6c0a5149f5338
Tanmay <cooltanmayvig@gmail.com>
2023-08-26 00:13:28 +0530

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-
-import './App.css'
-import Home from './pages/Home'
-import {Route , Routes} from 'react-router-dom'
+import "./App.css";
+import Home from "./pages/Home";
+import Details from "./pages/Details";
+import { Route, Routes } from "react-router-dom";
 function App() {
-  
-
-  return (
-    <>
-      <Routes>
-        <Route path='/' element={ < Home /> } />
-      </Routes>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/details" element={<Details />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,68 +1,91 @@
 import { auth, googleProvider } from "../config/firebase";
 import {
-  createUserWithEmailAndPassword,
-  signInWithPopup,
-  signOut,
+    createUserWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
 } from "firebase/auth";
 import { useState } from "react";
 
+import { useNavigate } from "react-router-dom";
+import pic2 from "../assets/avanti.png";
 
-import pic2 from '../assets/avanti.png'
-
-import './css-files/Home.css'
+import "./css-files/Home.css";
 
 export default function Home() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const signIn = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+    const signIn = async () => {
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-  const signInWithGoogle = async () => {
-    try {
-      await signInWithPopup(auth, googleProvider);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-  const logout = async () => {
-    try {
-      await signOut(auth);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  return (
-    <>
-      <br /> <br /> <br /> 
-    <div className="flex justify-center">
-      <div className="flex flex-col mx-20 text-center ">
-        <br /> <br />
-        <div className="text-white font-bold text-3xl">SIGN IN</div> <br />
-      <input
-        placeholder="Enter the Email" className=" px-7"
-        onChange={(e) => setEmail(e.target.value)}
-        /> <br />
-      <input
-        placeholder="Enter the Password" className=" px-7"
-        type="password"
-        onChange={(e) => setPassword(e.target.value)}
-        /> <br />
-      <button onClick={signIn} className="btn"> Sign In</button>
-      <br />
-      <button onClick={signInWithGoogle} className="btn"> Sign In Google
-      </button>
-      <br />
-      <button onClick={logout} className="btn"> Logout </button>
-        </div>
-        <img src={pic2} alt="avanti" height={100} width={700} />
-        </div>
-    </>
-  )
+    const logout = async () => {
+        try {
+            await signOut(auth);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+    const navigate = useNavigate();
+    return (
+        <>
+            <br /> <br /> <br />
+            <div className="flex justify-center">
+                <div className="flex flex-col mx-20 text-center ">
+                    <br /> <br />
+                    <form
+                        onSubmit={() => {
+                            navigate("/details");
+                        }}
+                    >
+                        <div className="text-white font-bold text-3xl">
+                            SIGN IN
+                        </div>{" "}
+                        <br />
+                        <input
+                            placeholder="Enter the Email"
+                            className=" px-7"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />{" "}
+                        <br />
+                        <input
+                            placeholder="Enter the Password"
+                            className=" px-7"
+                            type="password"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />{" "}
+                        <br />
+                        <button onClick={signIn} className=" bg-blue-700" type="submit">
+                            {" "}
+                            Sign In
+                        </button>
+                        <br />
+
+                        <button onClick={signInWithGoogle} className=" bg-blue-700" type="submit">
+                            {" "}
+                            Sign In Google
+                        </button>
+                        <br />
+                        <button onClick={logout} className=" bg-blue-700" type="submit">
+                            {" "}
+                            Logout{" "}
+                        </button>
+                    </form>
+                </div>
+                <img src={pic2} alt="avanti" height={100} width={700} />
+            </div>
+        </>
+    );
 }
